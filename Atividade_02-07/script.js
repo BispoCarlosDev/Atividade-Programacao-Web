337404105402a1f29a17422f0297fb0f8112020d
BispoCarlosDev <carlos.oliveirabispo@hotmail.com>
2024-02-20 00:48:46 -0300

    alteracao

---
+++ Atividade_02-07/script.js
@@ -0,0 +1,7 @@
+function limparTexto() {
+    document.getElementById("txtnome").value = "";
+    document.getElementById("txtsobrenome").value = "";
+    document.getElementById("txtcidade").value = "";
+    document.getElementById("txtemail").value = "";
+    document.getElementById("txttelefone").value = "";
+}
