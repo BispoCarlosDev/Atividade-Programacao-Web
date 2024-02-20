04595dbd9c4099cc345660e43c348d8665793d6b
BispoCarlosDev <carlos.oliveirabispo@hotmail.com>
2024-02-20 01:39:09 -0300

    alteracao

--- Atividade_02-07/script.js
+++ Atividade_02-07/script.js
@@ -1,7 +1,13 @@
-function limparTexto() {
-    document.getElementById("txtnome").value = "";
-    document.getElementById("txtsobrenome").value = "";
-    document.getElementById("txtcidade").value = "";
-    document.getElementById("txtemail").value = "";
-    document.getElementById("txttelefone").value = "";
+function limparTexto(id) {
+    if(id == 'txtnome'){
+        document.getElementById("txtnome").value = "";
+    }else if (id == 'txtsobrenome'){
+        document.getElementById("txtsobrenome").value = "";
+    }else if (id == 'txtcidade'){
+        document.getElementById("txtcidade").value = "";
+    }else if (id == 'txtemail'){
+        document.getElementById("txtemail").value = "";
+    }else if (id == 'txttelefone'){
+        document.getElementById("txttelefone").value = "";
+    }
 }
