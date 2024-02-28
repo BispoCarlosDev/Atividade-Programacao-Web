4b5d826904aaf6200a33c7d5f23e48d8f2f78388
BispoCarlosDev <carlos.oliveirabispo@hotmail.com>
2024-02-28 19:15:24 -0300

    alteracao

--- Atividade_02_21/Exercicios.js
+++ Atividade_02_21/Exercicios.js
@@ -1,7 +1,10 @@
-let nota01 = 10;
-let nota02 = 9;
-let nota03 = 8;
-let nota04 = 7;
-let media = (nota01 + nota02 + nota03 + nota04) / 4;
-
-document.write(`A Média das notas é.`)
+function calcular(){
+    function calcular(){
+        let nota01 = parseFloat(document.getElementById('nota01').value);
+        let nota02 = parseFloat(document.getElementById('nota02').value);
+        let nota03 = parseFloat(document.getElementById('nota03').value);
+        let nota04 = parseFloat(document.getElementById('nota04').value);
+        let media = (nota01 + nota02 + nota03 + nota04) / 4;
+        document.getElementById('media').innerText = `O valor da Média das notas é ${media}`;
+    }
+}
