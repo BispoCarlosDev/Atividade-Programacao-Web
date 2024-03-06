235b417ada681fc343bbb89e6483f1d535c6c2c1
BispoCarlosDev <carlos.oliveirabispo@hotmail.com>
2024-03-06 01:22:36 -0300

    exercicio 01

--- Atividade_02_21/Exercicios.js
+++ Atividade_02_21/Exercicios.js
@@ -1,10 +1,18 @@
-function calcular(){
-    function calcular(){
-        let nota01 = parseFloat(document.getElementById('nota01').value);
-        let nota02 = parseFloat(document.getElementById('nota02').value);
-        let nota03 = parseFloat(document.getElementById('nota03').value);
-        let nota04 = parseFloat(document.getElementById('nota04').value);
-        let media = (nota01 + nota02 + nota03 + nota04) / 4;
-        document.getElementById('media').innerText = `O valor da Média das notas é ${media}`;
-    }
-}
+const readlineSync = require('readline-sync');
+
+console.log("Digite a Nota do aluno na primeira matéria:");
+let nota01 = parseFloat(readlineSync.prompt());
+
+console.log("Digite a Nota do aluno na segunda matéria:");
+let nota02 = parseFloat(readlineSync.prompt());
+
+console.log("Digite a Nota do aluno na terceira matéria:");
+let nota03 = parseFloat(readlineSync.prompt());
+
+console.log("Digite a Nota do aluno na quarta matéria:");
+let nota04 = parseFloat(readlineSync.prompt());
+
+let soma = nota01 + nota02 + nota03 + nota04;
+let media = soma / 4;
+
+console.log("A Soma das notas do Aluno é " + soma + " e a média das notas do aluno é " + media + ".");
